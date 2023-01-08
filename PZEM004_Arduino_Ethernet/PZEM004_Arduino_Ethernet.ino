41cec31e5359d28dc8b7ba6a05e3dd2beb2f12d2
viki87@bk.ru <hG3a4RbdKY6qVYc5F6QX>
2023-01-08 19:49:06 +0300

    pzem memory fix

--- PZEM004_Arduino_Ethernet/PZEM004_Arduino_Ethernet.ino
+++ PZEM004_Arduino_Ethernet/PZEM004_Arduino_Ethernet.ino
@@ -12,74 +12,54 @@ byte server[] = { 192, 168, 1, 180 };
 
 EthernetClient ethClient;
 PubSubClient client(ethClient);
-String electroVoltage = "/electro/voltage1";
-String electroCurrent = "/electro/current1";
-String electroPower = "/electro/power1";
-String electroEnergy = "/electro/energy1";
-String electroFrequency = "/electro/frequency1";
-String electroPowerfactor = "/electro/powerfactor1";
-char* arduinoAliveTopic = "/alive/arduino/";
 SoftwareSerial pzemSWSerial(9, 8);
 PZEM004Tv30 pzem;
-
+char* arduinoAliveTopic = "/alive/arduino/";
 void setup()
 {    
   Serial.begin(9600);  
-  Serial.println("Setup started");
+  Serial.println("Setup>");
   initializeEthernet();
   initializeMQTT();
-  Serial.println("Setup finished");  
   pzem = PZEM004Tv30(pzemSWSerial);
+  Serial.println("Setup<");  
+  
   delay(500);//Wait for newly restarted system to stabilize
 }
 
 void initializeMQTT(){
-  Serial.println("MQTT initialize started");
+  Serial.println("MQTT>");
   client.setServer(server, 8083 );
-  client.setCallback(callback);
   
   if (client.connect(arduinoAliveTopic)) {
     sendMQTTMessage(arduinoAliveTopic, "OK");    
-    Serial.println("MQTT Working");
+    Serial.println("MQTT ok");
   }
   else{
-    Serial.println("MQTT not connected");
-  }
-  client.subscribe("/heat/batary1/turn");
-  Serial.println("MQTT initialize finished");
+    Serial.println("MQTT not ok");
+  }  
+  Serial.println("MQTT<");
 }
 
 void initializeEthernet(){
-  Serial.println("Ethernet initialize started");
+  Serial.println("Ethernet>");
   if (Ethernet.begin(mac) == 0) {
-    Serial.println("Failed to configure Ethernet using DHCP");
+    Serial.println("No ip");
     if (Ethernet.hardwareStatus() == EthernetNoHardware) {
-      Serial.println("Ethernet shield was not found.  Sorry, can't run without hardware. :(");
+      Serial.println("No ethernet shield");
     } else if (Ethernet.linkStatus() == LinkOFF) {
-      Serial.println("Ethernet cable is not connected.");
+      Serial.println("No cable");
     }    
   }else{
-    Serial.print("My IP address: ");
+    Serial.print("My IP: ");
     Serial.println(Ethernet.localIP());  
   }  
-  Serial.println("Ethernet initialize finished");
-}
-
-void callback(char* topic, byte* payload, unsigned int length) {
-  /*Example of receiving commands from MQTT*/
-  Serial.println("Message received");
-  payload[length] = '\0';
-  Serial.print(topic);
-  Serial.print("  ");
-  String strTopic = String(topic);
-  String strPayload = String((char*)payload);
-  Serial.println(strPayload);
+  Serial.println("Ethernet<");
 }
 
 void loop(void)
 {
-  checkConnections();
-  client.loop();
+  checkConnections();  
   
     // Read the data from the sensor
   float voltage = pzem.voltage();
@@ -88,35 +68,42 @@ void loop(void)
   float energy = pzem.energy();
   float frequency = pzem.frequency();
   float pf = pzem.pf();
+  String electroVoltage = "/electro/voltage1";
+  String electroCurrent = "/electro/current1";
+  String electroPower = "/electro/power1";
+  String electroEnergy = "/electro/energy1";
+  String electroFrequency = "/electro/frequency1";
+  String electroPowerfactor = "/electro/powerfactor1";
 
 
     // Check if the data is valid
   if(isnan(voltage)){
-      Serial.println("Error reading voltage");
+      Serial.println("Error voltage");
       sendMQTTMessage(electroVoltage, "-100");
   } else if (isnan(current)) {
-      Serial.println("Error reading current");
+      Serial.println("Error current");
       sendMQTTMessage(electroCurrent, "-100");      
   } else if (isnan(power)) {
-      Serial.println("Error reading power");
+      Serial.println("Error power");
       sendMQTTMessage(electroPower, "-100");
   } else if (isnan(energy)) {
-      Serial.println("Error reading energy");
+      Serial.println("Error energy");
       sendMQTTMessage(electroEnergy, "-100");
   } else if (isnan(frequency)) {
-      Serial.println("Error reading frequency");
+      Serial.println("Error frequency");
       sendMQTTMessage(electroFrequency, "-100");
   } else if (isnan(pf)) {
-      Serial.println("Error reading power factor");
+      Serial.println("Error power factor");
       sendMQTTMessage(electroPowerfactor, "-100");
   } else {
     // Print the values to the Serial console
+    /*
     Serial.print("Voltage: ");      Serial.print(voltage);      Serial.println("V");
     Serial.print("Current: ");      Serial.print(current);      Serial.println("A");
     Serial.print("Power: ");        Serial.print(power);        Serial.println("W");
     Serial.print("Energy: ");       Serial.print(energy,3);     Serial.println("kWh");
     Serial.print("Frequency: ");    Serial.print(frequency, 1); Serial.println("Hz");
-    Serial.print("PF: ");           Serial.println(pf);
+    Serial.print("PF: ");           Serial.println(pf);*/
     sendMQTTMessage(electroVoltage, String(voltage));
     sendMQTTMessage(electroCurrent, String(current));
     sendMQTTMessage(electroPower, String(power));
